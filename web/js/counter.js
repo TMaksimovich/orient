a15aca9e95e1ac92c55845eda9c144ff6c1e6e90
tatiana maksimovich <tatianamaksimovich@gmail.com>
2022-12-14 01:11:54 +0300

    front-page desktop

---
+++ web/js/counter.js
@@ -0,0 +1,9 @@
+import { CountUp } from './libs/countUp.js';
+
+let countName = document.querySelectorAll('#counter-value');
+
+for(let i = 0; i<=countName.length; i++){
+  let endVal = countName[i].dataset.valueCount;
+  countName[i].classList.add("animate-active");
+  new CountUp(countName[i], endVal, { enableScrollSpy: true, scrollSpyOnce: true });
+}

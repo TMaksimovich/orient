51d15da9dbe7afccf068372a3dd383efdbc38045
tatiana maksimovich <tatianamaksimovich@gmail.com>
2022-12-17 01:11:42 +0300

    counter circle animation

--- web/js/counter.js
+++ web/js/counter.js
@@ -4,6 +4,26 @@ let countName = document.querySelectorAll('#counter-value');
 
 for(let i = 0; i < countName.length; i++){
   let endVal = countName[i].dataset.valueCount;
-  countName[i].classList.add("animate-active");
-  new CountUp(countName[i], endVal, { enableScrollSpy: true, scrollSpyOnce: true });
+  new CountUp(countName[i], endVal, { enableScrollSpy: true, scrollSpyOnce: true, scrollSpyDelay: 2 });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	const scrollItems = document.querySelectorAll('.counter-wrapper');
+
+	const scrollAnimation = () => {
+		let windowCenter = (window.innerHeight / 2) + window.scrollY;
+		//console.log(windowCenter)
+		scrollItems.forEach(el => {
+			let scrollOffset = el.offsetTop + (el.offsetHeight / 2);
+			//console.log(scrollOffset)
+			if (windowCenter >= scrollOffset - 300) {
+				el.classList.add('animate-active');
+			} 
+		});
+	};
+
+	scrollAnimation();
+	window.addEventListener('scroll', () => {
+		scrollAnimation();
+	});
+});
